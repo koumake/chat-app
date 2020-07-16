d33b65800a7b6659a60121fa4e650a07fa6b124c
koumake <koumake422@gmail.com>
2020-07-16 15:32:46 +0900

    プルダウンの実装

--- db/migrate/20200716025543_create_room_users.rb
+++ db/migrate/20200716025543_create_room_users.rb
@@ -1,6 +1,6 @@
 class CreateRoomUsers < ActiveRecord::Migration[6.0]
   def change
-    create_table :room_users do |t|
+    create_table :room_uesrs do |t|
       t.references :room, foreign_key: true
       t.references :user, foreign_key: true
       t.timestamps
